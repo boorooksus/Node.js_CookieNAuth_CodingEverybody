01886f28fe0d30ad01a558ea8d9903b618bbc5e8
boorooksus <boorooksus@gmail.com>
2020-07-30 14:44:02 +0900

    Create 3_cookie.js

---
+++ nodejs/3_cookie.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function(request, response){
+    response.writeHead(200, {
+        // 쿠키 생성. 여러 개의 쿠키를 생성할 땐 배열 사용
+        'Set-Cooie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    });
+    response.end('Cookie')
+}).listen(3000);
