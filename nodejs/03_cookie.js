f2234566ea3633b13849a30e15659b2dd7d27557
boorooksus <boorooksus@gmail.com>
2020-08-01 13:59:50 +0900

    update 03_cookie.js

--- nodejs/03_cookie.js
+++ nodejs/03_cookie.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function(request, response){
     response.writeHead(200, {
         // 쿠키 생성. 여러 개의 쿠키를 생성할 땐 배열 사용
-        'Set-Cooie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
     });
     response.end('Cookie')
 }).listen(3000);
